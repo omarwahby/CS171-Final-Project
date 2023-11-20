27b64de58bc3a570823ff7f2f710c2b54672e57c
Samuel Osa-Agbontaen <sosaagbontaen@college.harvard.edu>
2023-11-20 01:36:09 -0500

    Added scatterplot

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@ let dateParser = d3.timeParse("%Y-%m-%d");
 // (1) Load data with promises
 
 let promises = [
-    d3.json("data/perDayData.json"),
-    d3.json("data/myWorldFields.json")
+    d3.csv("data/satvscomprate.csv")
 ];
 
 Promise.all(promises)
@@ -19,51 +18,27 @@ Promise.all(promises)
     });
 
 function createVis(data) {
-    let perDayData = data[0]
-    let metaData = data[1]
-    // error, perDayData, metaData
-    // if(error) { console.log(error); }
+    let perSchoolData = data[0]
 
-    console.log(data)
-    // (2) Make our data look nicer and more useful
-    allData = perDayData.map(function (d) {
+    // Make our data look nicer and more useful
+    allData = perSchoolData.map(function (d) {
 
-        let result = {
-            time: dateParser(d.day),
-            count: +d["count(*)"] + 1
+        let schoolObject = {
+            schoolID: +d.UNITID,
+            avg_sat: +d.SAT_AVG,
+            comp_rate: +d.C100_4
         };
-
-        // Convert votes for the 15 priorities from key-value format into one single array (for each day)
-        result.priorities = d3.range(0, 15).map(function (counter) {
-            return d["sum(p" + counter + ")"]
-        });
-        // [d["sum(p0)"], d["sum(p1)"], d["sum(p2)"],...]
-        // Example: [10,200,500,... ]
-
-        // Create an array of values for age 0 - 99
-        result.ages = d3.range(0, 99).map(function () {
-            return 0;
-        });
-
-        // Insert the votes in the newly created array 'result.ages'
-        d.age.forEach(function (a) {
-            if (a.age < 100) {
-                result.ages[a.age] = a["count(*)"];
-            }
-        })
-
-        return result;
+        return schoolObject;
     });
+    // console.log(allData)
 
 
     // (3) Create event handler
     // *** TO-DO ***
 
     // (4) Create visualization instances
-    let countVis = new CountVis("scatterplotvis", allData);
-    let countVis1 = new CountVis("piechartvis", allData);
-    let countVis2 = new CountVis("mapvis", allData);
-    let countVis3 = new CountVis("debtvis", allData);
+    let scatterplotVis = new ScatterPlotVis("scatterplotvis", allData);
+    let pictogramVis = new PictoGramVis("pictogramvis", allData);
 
     // *** TO-DO ***
     //  pass event handler to CountVis, at constructor of CountVis above
