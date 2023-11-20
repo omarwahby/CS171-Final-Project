f47202f177525632a46a6591bf52bdfd8c64947a
Omar Wahby <owahby@college.harvard.edu>
2023-11-20 00:48:44 -0500

    simplified bar chart code

--- js/barchart.js
+++ js/barchart.js
@@ -1,146 +1,101 @@
-// define margins and drawing area
-let margin = {top: 40, right: 60, bottom: 60, left: 60};
-
-let width = 1200 - margin.left - margin.right,
+// define margins
+// wanted the area chart to be a bit bigger than the bar chart area, so two widths and heights were defined
+let margin = {
+        top: 10,
+        right: 35,
+        bottom: 50,
+        left: 100},
+    width = 850 - margin.left - margin.right,
     height = 650 - margin.top - margin.bottom;
 
-let svg = d3.select("#chart-area")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// define time parser
+let parseTime= d3.timeParse("%Y-%m-%d");
 
-var padding = 10;
-// Scales
-let x = d3.scaleBand()
-    .rangeRound([padding, width])
-    .paddingInner(0.3);
+// journey to the promise land
+d3.csv("data/zaatari-refugee-camp-population.csv", d => {
+    // parse data properly
+    d.population = +d.population;
+    d.date = parseTime(d.date);
+    return d;
 
-let y = d3.scaleLinear()
-    .range([height, 0]);
-
-// Initialize variables
-var initialValue = d3.select("#ranking-type").property("value");
-var toggle = true;
-
-// Initialize data
-loadData();
-
-// Create a 'data' property under the window object
-// to store the coffee chain data
-Object.defineProperty(window, 'data', {
-    // data getter
-    get: function() { return _data; },
-    // data setter
-    set: function(value) {
-        _data = value;
-        // update the visualization each time the data property is set by using the equal sign (e.g. data = [])
-        updateVisualization(initialValue, toggle)
-    }
+}).then(function(data){
+    // draw area chart
+    drawAreaChart(data);
+    // draw bar chart
+    drawBarChart(data);
 });
 
-// Load CSV file
-function loadData() {
-    d3.csv("data/coffee-house-chains.csv").then(csv=> {
+function drawBarChart(data){
 
-        csv.forEach(function(d){
-            d.revenue = +d.revenue;
-            d.stores = +d.stores;
-        });
+    // define shelter data
+    const shelterData = [
+        { type: "Caravans", percentage: 79.68 },
+        { type: "Combination*", percentage: 10.81 },
+        { type: "Tents", percentage: 9.51 }
+    ];
 
-        // Store csv data in global variable
-        data = csv;
-    });
-}
+    let padding = 30;
 
-// Render visualization
-function updateVisualization(selectedValue) {
+    // define svg area of BAR CHART using said margins
+    let barChartSvg = d3.select("#bar-chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + (margin.left) + "," + margin.top+ ")");
 
-    // redefine the data to only include the selected parameter (stores v revenue)
-    var dataFilter = data.map(function(d){return {company: d.company, value: d[selectedValue]}});
+    // Create an ordinal scale for the x-axis
+    const xBarScale = d3.scaleBand()
+        .domain(shelterData.map(d => d.type))
+        .range([padding, width - padding])
+        .padding(0.2);
+    // Add x-axis
+    barChartSvg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + (height-2*padding) + ")")
+        .call(d3.axisBottom().scale(xBarScale))
+        .style("font-size", "12px");
 
-    //function to sort and return data;
-    sortedData = sortData(dataFilter,toggle);
+    // Create y-scale for the bar chart
+    const yBarScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height - 2*padding, 2*padding]);
+    // Add y-axis
+    barChartSvg.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(d3.axisLeft().scale(yBarScale))
+        .style("font-size", "12px");
 
-    //update domains of scales
-    y.domain([0, d3.max(sortedData, d=> d.value)]);
-    x.domain(sortedData.map(d => d.company));
+    // Draw bars
+    barChartSvg.selectAll(".bar")
+        .data(shelterData)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xBarScale(d.type))
+        .attr("y", d => yBarScale(d.percentage))
+        .attr("width", xBarScale.bandwidth())
+        .attr("height", d => height - yBarScale(d.percentage) -60)
+        .style("font-size", "12px");
 
-    // define svg area
-    let bar = svg.selectAll("rect")
-        .data(sortedData);
-
-    // create x axis, y axis, and chart title
-    var xAxis = svg.append("g")
-        .attr("class", "xaxis")
-        .attr("transform", "translate(0,"+ (height) +")");
-
-    var yAxis = svg.append("g")
-        .attr("class", "yaxis");
-
-    svg.append("text")
-        .attr("class", "title")
-        .attr("x", (padding))
-        .attr("y", padding-(margin.top / 2))
+    // Add labels above each bar
+    barChartSvg.selectAll(".bar-label")
+        .data(shelterData)
+        .enter().append("text")
+        .attr("class", "bar-label")
+        .attr("x", d => xBarScale(d.type) + xBarScale.bandwidth() / 2)
+        .attr("y", d => yBarScale(d.percentage) - 5)
         .attr("text-anchor", "middle")
-        .style("font-size", "12px")
-        .style("opacity", "0.8")
-        .text("Optional Title");
+        .text(d => `${d.percentage.toFixed(2)}%`)
+        .style("font-size", "12px");
 
-    // exit
-    bar.exit().remove();
-
-    // enter and update
-    bar.enter().append("rect")
-        .merge(bar)
-        .transition()
-        .duration(2000)
-        .attr("class", "mybar")
-        .attr("x", function (d) { return x(d.company);} )
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return (height-y(d.value)); })
-        .attr("width", x.bandwidth() )
-        .attr("fill", "#6E4B2B")
-        .attr("stroke", "#6E4B2B")
-        .attr("stroke-width", "4");
-
-    // update x axis
-    svg.selectAll('.xaxis')
-        .transition()
-        .duration(1000)
-        .call(d3.axisBottom(x));
-
-    // update y axis
-    svg.selectAll('.yaxis')
-        .transition()
-        .duration(1000)
-        .call(d3.axisLeft(y));
-
-    // update title
-    svg.selectAll('.title')
-        .transition()
-        .duration(500)
-        .text(selectedValue)
-
-    // when the drop-down options are changed, update the value param and recall the graphing function
-    d3.select("#ranking-type").on("change", function(d){
-        var newData = d3.select("#ranking-type").property("value");
-        updateVisualization(newData);
-    });
-
-    // when the sort button is clicked, toggle the toggle and recall the graphing function
-    d3.select("#change-sorting").on("click", function() {
-        toggle = !toggle;
-        updateVisualization(selectedValue);
-    });
-
-}
-
-function sortData(data,toggle){
-    if (toggle){
-        return data.sort((a,b)=> b.value - a.value);
-    } else {
-        return data.sort((a,b)=> a.value - b.value);
-    }
+    // Add chart title
+    barChartSvg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", width / 2)
+        .attr("y", 30)
+        .style("text-anchor", "middle")
+        .style("text-decoration", "underline")
+        .text("Type of Shelter")
+        .style("font-size", "20px");
 }
