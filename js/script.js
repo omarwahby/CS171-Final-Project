cb0f74eaa8c1b696b9a9b2395ea657d70da666d2
Omar Wahby <owahby@college.harvard.edu>
2023-12-03 16:11:51 -0500

    changed all background slide colors to white

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ new fullpage('#fullpage', {
   //options here
   autoScrolling:true,
   scrollHorizontally: true,
-  sectionsColor : ['#fe5f46', '#0998ec', '#fc6c7c', '#fec304'],
+  sectionsColor : ['#ffffff', '#ffffff', '#ffffff', '#ffffff'],
   navigation: true,
   slidesNavigation: true,
   anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
